5aac7a3feb596f76b4ebde7da05a3928163fa436
Geon-qr5 <zzzqr5@gmail.com>
2024-07-16 16:28:25 +0900

    JAVA 2일차 마무리

--- src/App.java
+++ src/App.java
@@ -2,4 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    // https://github.com/momoclass5/javaExample.git
 }
---
+++ src/com/human/A_App.java
@@ -0,0 +1,21 @@
+package com.human;
+
+import com.human.a_variable.VariableType;
+
+public class A_App {
+    public static void main(String[] args) {
+        VariableType v = new VariableType();
+        v.type1();
+        System.out.println("================================================");
+        v.type2();
+        System.out.println("================================================");
+        v.type3();
+        System.out.println("================================================");
+        v.type4();
+        System.out.println("================================================");
+        v.type5();
+        System.out.println("================================================");
+        v.type6();
+    }
+
+}
